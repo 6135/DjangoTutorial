4ff14ef365f78c0ca0913ee47c5157c5783220a8
6135 <guibscosta@hotmail.com>
2020-02-28 15:07:28 +0000

    Django

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Test(models.Model):
+    test_title = models.CharField(max_length=200)
+    test_content = models.TextField()
+    test_published = models.DateTimeField('published at')
+
+    def __str__(self):
+        return self.test_title
+        
+    
