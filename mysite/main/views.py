4e9eac244d5efaec2e9cd33dcef3d22bc6b6fe34
6135 <guibscosta@hotmail.com>
2020-03-03 00:53:59 +0000

    New stuff

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,10 +5,35 @@ from .models import *
 
 # Create your views here.
 
-def homepage(request):
-    return render(request = request,
-    template_name='main/index_template.html',
-    context = {'products': Product.objects.all, 'Categories': Category.objects.all}
-    )
+def login(request):
 
-    #test commit
+    return render(request = request,
+        template_name='main/login/login_template.html',
+        context = {'cart': 1},
+        )
+
+def homepage(request):
+    cart = True
+    return render(request = request,
+        template_name='main/index_template.html',
+        context = {
+            'cart': cart,
+            'cartM': {"name": "Cart", "href": "#"},
+            'products': Product.objects.all, 
+            'Categories': Category.objects.all,
+            'Menus': [{
+                "name": "Ola", "href": "#"
+                },{
+                "name": "Ola2","href": "#"
+                },
+            ]
+        })
+
+def catPage(request, cat_id = None):
+    if cat_id != None:
+        print("no cat selected\n")
+    else:
+        print(cat_id)
+    return render(request,
+        template_name = 'main/index_template.html',
+        context = {})
